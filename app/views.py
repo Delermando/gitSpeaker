af5de4e45dcef4465cb4c8d74d4b1c3e0ad4677a
Dell <delsantos@hotmail.com.br>
2015-09-04 01:36:45 -0300

    removendo arquivos da instalacao do python e pip

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from flask import render_template
 from app import app
+from github import Github
+import base64
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    content = gitFileGetContent('gitSpeaker', 'server.py')
+    return render_template('index.html', content = content)
+
+
+def gitFileGetContent(repository, filename):
+    github = Github('delermando', 'Aslam1618033989')
+    user = github.get_user()
+    repository = user.get_repo( repository )
+    gitFile = repository.get_contents( filename )
+    return base64.b64decode(gitFile.content)
