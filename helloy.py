bfbbdf1301106e4e33dd95b047e98b825f17adb2
Delermando <delsantos@hotmail.com.br>
2015-09-04 19:18:56 -0300

    adicionando hello

---
+++ helloy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello World</hi>"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
