eb75c3245b9a903104491c371ea159ebe8161a9d
Delermando <delsantos@hotmail.com.br>
2015-09-05 00:02:46 -0300

    aplicando modificaçoes nos arquivos de configuração e rotas

--- app/views.py
+++ app/views.py
@@ -6,14 +6,13 @@ import os
 
 @app.route("/")
 def index():
-    #content = gitFileGetContent(os.environ.get('GITSPEAKER_GH_REPOSITORYNAME'), os.environ.get('GITSPEAKER_GH_FIRSTFILENAME'))
-    content = 'teste'
+    content = gitFileGetContent(os.environ.get('GITSPEAKER_GH_REPOSITORYNAME'), os.environ.get('GITSPEAKER_GH_FIRSTFILENAME'))
     return render_template('index.html', content = content)
 
 
-def gitFileGetContent(repository, filename):
+def gitFileGetContent(repositoryName, filename):
     github = Github(os.environ.get('GITSPEAKER_GH_USERNAME'), os.environ.get('GITSPEAKER_GH_PASSWORD'))
     user = github.get_user()
-    repository = user.get_repo( repository )
+    repository = user.get_repo( repositoryName )
     gitFile = repository.get_contents( filename )
     return base64.b64decode(gitFile.content)
