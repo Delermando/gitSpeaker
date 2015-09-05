261f00352e3dd91a1b24c74a9fca2054570e48f9
Delermando <delsantos@hotmail.com.br>
2015-09-05 15:19:13 -0300

    aplicando suporte a markdown

--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
 from flask import render_template
+from flask import Markup
 from app import app
 from github import Github
 import base64
 import os
+import misaka as m 
 
 @app.route("/")
 def index():
     content = gitFileGetContent(os.environ.get('GITSPEAKER_GH_REPOSITORYNAME'), os.environ.get('GITSPEAKER_GH_FIRSTFILENAME'))
-    return render_template('index.html', content = content)
+    content = m.html(content,extensions=m.EXT_STRIKETHROUGH)
+    return render_template('index.html', content = Markup(content))
 
 
 def gitFileGetContent(repositoryName, filename):
