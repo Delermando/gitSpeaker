43bb23e81cf3e815b97ba5e35e4f677aed6540c7
Delermando <delsantos@hotmail.com.br>
2015-09-04 23:34:51 -0300

    testando procfile

--- helloy.py
+++ helloy.py
@@ -9,4 +9,5 @@ def index():
     return "<h1>Hello World</hi>"
 
 if __name__ == "__main__":
-    app.run(debug=True, port = int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
