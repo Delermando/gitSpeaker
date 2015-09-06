237f49715d0ca5877c3fa8ca6cb639ed8d29db23
Dell <delsantos@hotmail.com.br>
2015-09-06 01:43:40 -0300

    suport to sintax highlight

--- teste.py
+++ teste.py
@@ -1,113 +1,11 @@
-content = """
- * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
-# Imagine, Create and Refactor #
-
-## Introduction ##
-This is an initiative to create a tool that helps developers improve their codes of practice and rapid way. This is a first version of this tool initially focused on PHP, but which will be expanded to other programmming languages and more features.  
-
-
-## Features ##
-1. Mark Not Used Objects **( ctrl + shift + x )**
-    - Marks the line of a private function or variable not used in the current file / class
-
-2. **It is in development!** Extract Function From Selection **( ctrl + shift + b )**
-    - Extract the selected code to a new function
-
-2. **Next functionality** (Identify unused variables in a function)
-    - Mark unused variables in a functions
-
-## Suggested Features ##
-Feel free  to suggest new features for this tool through **delsantos@hotmail.com.br** email
-
-## Installation ##
-####Automatic Installation####
-It is necessary to have previously installed the Package Controll  
-1. Access Package controll using the shortcut (ctrl + shift + p) and type "Install Package"  
-2. Wait until you open a search bar and type Refactor and press Enter  
-
-####Manual Installation####
-It must have previously installed the Git and SublimeText  
-1. Clone the following repository <pre><code>git clone git@github.com:Delermando/Refactor.git</code></pre>
-2. Move the folder Refactor to the Sublime Packages folder  
-
-License
-=======
-
-MIT and GPL license.
-
-Copyright (c) 2013 Stephan Ahlf <stephan@ahlf-it.de>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-
-
-172.17.42.1 - - [05/Sep/2015 18:55:52] "GET / HTTP/1.1" 200 -
-# Imagine, Create and Refactor #
-
-## Introduction ##
-This is an initiative to create a tool that helps developers improve their codes of practice and rapid way. This is a first version of this tool initially focused on PHP, but which will be expanded to other programmming languages and more features.  
-
-
-## Features ##
-1. Mark Not Used Objects **( ctrl + shift + x )**
-    - Marks the line of a private function or variable not used in the current file / class
-
-2. **It is in development!** Extract Function From Selection **( ctrl + shift + b )**
-    - Extract the selected code to a new function
-
-2. **Next functionality** (Identify unused variables in a function)
-    - Mark unused variables in a functions
-
-## Suggested Features ##
-Feel free  to suggest new features for this tool through **delsantos@hotmail.com.br** email
-
-## Installation ##
-####Automatic Installation####
-It is necessary to have previously installed the Package Controll  
-1. Access Package controll using the shortcut (ctrl + shift + p) and type "Install Package"  
-2. Wait until you open a search bar and type Refactor and press Enter  
-
-####Manual Installation####
-It must have previously installed the Git and SublimeText  
-1. Clone the following repository <pre><code>git clone git@github.com:Delermando/Refactor.git</code></pre>
-2. Move the folder Refactor to the Sublime Packages folder  
-
-License
-=======
-
-MIT and GPL license.
-
-Copyright (c) 2013 Stephan Ahlf <stephan@ahlf-it.de>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-
-
-"""
-
-
-rows = content.splitlines()
-contentRowsNumber = content.count('\n')
-
-
 def groupRows( rows, rowsNumber, contentRowsNumber):
+    groups = []
+    group = ''
     times = (contentRowsNumber/rowsNumber)
     if (contentRowsNumber % rowsNumber ) != 0:
         times += 1
-    groups = []
-    group = ''
-    for time in range(0, contentRowsNumber, times):
-        
+
+    for time in range(0, contentRowsNumber, rowsNumber):
         for row in rows[ time : time + rowsNumber]:
             group += row + '\n'
         groups.append(group)
@@ -115,6 +13,23 @@ def groupRows( rows, rowsNumber, contentRowsNumber):
     return groups
 
 
+content = r"""
+        !/usr/bin/env python
+    
+        if ( ! current_user_can('manage_links') )
+            wp_link_manager_disabled_message();
+    
+        if ( !empty($_POST['deletebookmarks']) )
+            $action = 'deletebookmarks';
+        if ( !empty($_POST['move']) )
+            $action = 'move';
+        if ( !empty($_POST['linkcheck']) )
+            $linkcheck = $_POST['linkcheck'];
+        if ( !empty($_POST['linkcheck']) )  
+            $linkcheck = $_POST['linkcheck'];
+        """
 
-g = groupRows(rows, 10, contentRowsNumber)
-print(g[1])
+#content = gitFileGetContent(os.environ.get('GITSPEAKER_GH_REPOSITORYNAME'), os.environ.get('GITSPEAKER_GH_FIRSTFILENAME'))
+contentGroups = groupRows(content.splitlines(), 10, content.count('\n'))
+print(len(contentGroups))
+
