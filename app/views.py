df00103997b75fee852472cd8e03ffdc8e531b28
Delermando <delsantos@hotmail.com.br>
2015-09-08 00:08:52 -0300

    refactor in function of calc better position to cut

--- app/views.py
+++ app/views.py
@@ -5,13 +5,15 @@ from app import *
 @app.route("/")
 def index():
     var = Tools.getEnviromentVar()
-    user = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
-    repository = Git.getRepository(user, var['GITSPEAKER_GH_REPOSITORYNAME'])
-    dirContent = Git.getDirContentFromRepository(repository, '/')
+    #user = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
+    #repository = Git.getRepository(user, var['GITSPEAKER_GH_REPOSITORYNAME'])
+    #dirContent = Git.getDirContentFromRepository(repository, '/')
 
     formatedContent = formatFileContent(
-        dirContent[6].name, 
-        base64.b64decode(dirContent[6].content), 
+        #dirContent[6].name, 
+        #base64.b64decode(dirContent[6].content), 
+        var['GITSPEAKER_GH_FIRSTFILENAME'], 
+        var['GITSPEAKER_GH_FILECONTENT'], 
         int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), 
         int(var['GITSPEAKER_GH_CODELINESNUMBER'])
     )
@@ -22,9 +24,7 @@ def index():
 def formatFileContent(filename, content, markdownLinesNumber, codeLinesNumber):
     extension = Tools.getFileExtension(filename)
     if(extension == 'md'):
-        numberOfLines = markdownLinesNumber 
+        contentGroups = Content.rowsMdFiles(content.splitlines(), markdownLinesNumber, content.count('\n'))
     else:
-        numberOfLines = codeLinesNumber
-
-    contentGroups = Content.rows(content.splitlines(), numberOfLines, content.count('\n'))
+        contentGroups = Content.rowsCodeFiles(content.splitlines(), codeLinesNumber, content.count('\n'))
     return Section.set(contentGroups, Section.getPatter(extension))
