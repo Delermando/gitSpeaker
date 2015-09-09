19ca754d7f6404cdcbfce38679a8861ceff2bd7f
Delermando <delsantos@hotmail.com.br>
2015-09-09 02:34:25 -0300

    create suport to render a path to a file

--- app/core/git.py
+++ app/core/git.py
@@ -6,10 +6,20 @@ class Git(object):
 
     def getGithubUser(self, userName, password):
         github = self.Github(userName, password)
-        return github.get_user()    
+        return github.get_user()   
 
     def getRepository(self, user, repositoryName):
         return user.get_repo( repositoryName )
 
     def getDirContentFromRepository(self,repository, dirName):
         return repository.get_dir_contents( dirName )
+
+    def getTree(self,repository, shaCommit, isRecursive):
+        fileList = []
+        repoTree = repository.get_git_tree(commitSha,isRecursive)
+        for fl in repoTree.tree:
+            fileList.append(fl)
+        return fileList
+
+    def getBranch(self, repository, branchName ):
+        return repository.get_branch( branchName )
--- app/views.py
+++ app/views.py
@@ -2,18 +2,39 @@ from flask import render_template
 from flask import Markup
 import base64
 from app import *
+
+
+@app.route("/get/<username>/<repository>/<folder>/<fname>")
+@app.route("/get/<username>/<repository>/<folder>")
+@app.route("/get/<username>/<repository>")
+def slidePath(username,repository,folder = False, fname = False):
+    var = Tools.getEnviromentVar()
+    user = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
+    repository = Git.getRepository(user, repository)
+    dirContent = Git.getDirContentFromRepository(repository, folder)
+    for fl in dirContent:
+        if  fl.name == fname:
+            return render_template('index.html', content = Markup(formatFileContent(
+                fl.name, 
+                base64.b64decode(fl.content), 
+                int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), 
+                int(var['GITSPEAKER_GH_CODELINESNUMBER'])
+            )));
+
+    return render_template('index.html', content = "Arquivo nao encontrado" )
+
 @app.route("/")
 def index():
     var = Tools.getEnviromentVar()
-    user = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
-    repository = Git.getRepository(user, var['GITSPEAKER_GH_REPOSITORYNAME'])
-    dirContent = Git.getDirContentFromRepository(repository, '/')
+    #user = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
+    #repository = Git.getRepository(user, var['GITSPEAKER_GH_REPOSITORYNAME'])
+    #dirContent = Git.getDirContentFromRepository(repository, '/')
 
     formatedContent = formatFileContent(
-        dirContent[6].name, 
-        base64.b64decode(dirContent[6].content), 
-        #var['GITSPEAKER_GH_FIRSTFILENAME'], 
-        #var['GITSPEAKER_GH_FILECONTENT'], 
+        #dirContent[6].name, 
+        #base64.b64decode(dirContent[6].content), 
+        var['GITSPEAKER_GH_FIRSTFILENAME'], 
+        var['GITSPEAKER_GH_FILECONTENT'], 
         int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), 
         int(var['GITSPEAKER_GH_CODELINESNUMBER'])
     )
