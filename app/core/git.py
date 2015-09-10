59ed9d8cd496b4ad222282f115cc0a45f4c0f111
Delermando <delsantos@hotmail.com.br>
2015-09-09 22:16:50 -0300

    criando funcoes para buscar repositorios publicos

--- app/core/git.py
+++ app/core/git.py
@@ -5,21 +5,64 @@ class Git(object):
         self.Github = Github
 
     def getGithubUser(self, userName, password):
-        github = self.Github(userName, password)
-        return github.get_user()   
+        return self.Github(userName, password)
+        #return github.get_user()   
+
+    def extractUserRepoInfo(self, path ):
+        result = {}
+        gitPath = path.split('/')
+        result['user'] = gitPath[0]
+        result['repository'] = gitPath[1]
+        result['path'] = '/'.join(gitPath[2:])
+        return result
+
+    def getSearcUser(self,git, username ):
+        git.legacy_search_users( username )
+        try:
+            searchUser = git.legacy_search_users( username )
+            counter = 0
+            for user in searchUser:
+                if counter >= 2:
+                    return False
+                counter += 1
+            return user
+        except Exception:
+            return False
+
 
     def getRepository(self, user, repositoryName):
-        return user.get_repo( repositoryName )
+        try:
+            return user.get_repo( repositoryName )
+        except Exception:
+            return False
 
-    def getDirContentFromRepository(self,repository, dirName):
+    def getDirContentFromRepository(self, repository, dirName):
         return repository.get_dir_contents( dirName )
 
-    def getTree(self,repository, shaCommit, isRecursive):
+    def getBranch(self, repository, branchName ):
+        return repository.get_branch( branchName )
+
+    def getTree(self, repository, shaCommit, isRecursive):
         fileList = []
-        repoTree = repository.get_git_tree(commitSha,isRecursive)
+        repoTree = repository.get_git_tree(shaCommit,isRecursive)
         for fl in repoTree.tree:
             fileList.append(fl)
         return fileList
 
-    def getBranch(self, repository, branchName ):
-        return repository.get_branch( branchName )
+    def extractFileListInfo(self, tree):
+        fileDict = {}
+        for fl in tree:
+            fileDict[fl.path] = fl.type
+        return fileDict
+
+    def checkFile(self, fileDict, filePath):
+        if fileDict.has_key(filePath):
+            return fileDict[filePath]
+        else:
+            return False
+
+    def getFileContent(self, repository, path):
+        return repository.get_file_contents( path )
+
+    def getDirContentFromRepository(self, repository, dirName):
+        return repository.get_dir_contents( dirName )
