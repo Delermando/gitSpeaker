3262fda12d0502bbfe930874d101db3244316db6
Delermando <delsantos@hotmail.com.br>
2015-09-09 22:17:37 -0300

    criando rota para carregar repositorio do git com base na rota

--- app/views.py
+++ app/views.py
@@ -3,43 +3,40 @@ from flask import Markup
 import base64
 from app import *
 
-
-@app.route("/get/<username>/<repository>/<folder>/<fname>")
-@app.route("/get/<username>/<repository>/<folder>")
-@app.route("/get/<username>/<repository>")
-def slidePath(username,repository,folder = False, fname = False):
-    var = Tools.getEnviromentVar()
-    user = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
-    repository = Git.getRepository(user, repository)
-    dirContent = Git.getDirContentFromRepository(repository, folder)
-    for fl in dirContent:
-        if  fl.name == fname:
-            return render_template('index.html', content = Markup(formatFileContent(
-                fl.name, 
-                base64.b64decode(fl.content), 
-                int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), 
-                int(var['GITSPEAKER_GH_CODELINESNUMBER'])
-            )));
-
-    return render_template('index.html', content = "Arquivo nao encontrado" )
-
 @app.route("/")
-def index():
+def apresentation():
+    return render_template('apresentation.html')
+
+@app.route('/get/', defaults={'path': ''})
+@app.route('/get/<path:path>')
+def slide(path):
     var = Tools.getEnviromentVar()
-    #user = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
-    #repository = Git.getRepository(user, var['GITSPEAKER_GH_REPOSITORYNAME'])
-    #dirContent = Git.getDirContentFromRepository(repository, '/')
+    urlParams = Git.extractUserRepoInfo(path)
+    branchName = 'master'
+    response = ''
 
-    formatedContent = formatFileContent(
-        #dirContent[6].name, 
-        #base64.b64decode(dirContent[6].content), 
-        var['GITSPEAKER_GH_FIRSTFILENAME'], 
-        var['GITSPEAKER_GH_FILECONTENT'], 
-        int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), 
-        int(var['GITSPEAKER_GH_CODELINESNUMBER'])
-    )
+    git = Git.getGithubUser(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'])
+    user = Git.getSearcUser(git, urlParams['user'] )
+    repository = Git.getRepository(user , urlParams['repository'])
+    print(repository,urlParams['repository'])
+    branch = Git.getBranch( repository, branchName)
+    tree = Git.getTree(repository, branch.commit.sha, True)
+    fileDict = Git.extractFileListInfo(tree)
+    fl = Git.checkFile(fileDict, urlParams['path'])
 
-    return render_template('index.html', content = Markup( formatedContent ))
+
+    if fl == 'blob':
+        gitFl = Git.getFileContent(repository, urlParams['path'])
+        response = Markup(formatFileContent(gitFl.name, base64.b64decode(gitFl.content), int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), int(var['GITSPEAKER_GH_CODELINESNUMBER']) ))
+    elif fl == 'tree':
+        gitDir = Git.getDirContentFromRepository(repository, urlParams['path'])
+        for gitFl in gitDir:
+            response += Markup(formatFileContent(gitFl.name, base64.b64decode(gitFl.content), int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), int(var['GITSPEAKER_GH_CODELINESNUMBER']) ))
+    else:
+        response = 'Nao existe'
+
+    
+    return render_template('slide.html', content = response)
 
 
 def formatFileContent(filename, content, markdownLinesNumber, codeLinesNumber):
