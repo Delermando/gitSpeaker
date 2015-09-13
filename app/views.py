edcfbcc29cb687be1aacb0df41a9300330619fc3
Delermando <delsantos@hotmail.com.br>
2015-09-13 15:11:38 -0300

    solve

--- app/views.py
+++ app/views.py
@@ -2,15 +2,16 @@ from flask import render_template
 from app import app
 from github import Github
 import base64
+import os
 
 @app.route("/")
 def index():
-    content = gitFileGetContent('gitSpeaker', 'server.py')
+    content = gitFileGetContent(os.environ.get('GITSPEAKER_GH_REPOSITORYNAME'), os.environ.get('GITSPEAKER_GH_FIRSTFILENAME'))
     return render_template('index.html', content = content)
 
 
 def gitFileGetContent(repository, filename):
-    github = Github('delermando', 'Aslam1618033989')
+    github = Github(os.environ.get('GITSPEAKER_GH_USERNAME'), os.environ.get('GITSPEAKER_GH_PASSWORD'))
     user = github.get_user()
     repository = user.get_repo( repository )
     gitFile = repository.get_contents( filename )
