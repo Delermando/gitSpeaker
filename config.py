6d4da6f714d32792ae259515ddc805730d417449
Delermando <delsantos@hotmail.com.br>
2015-09-27 17:31:21 -0300

    fix db connection

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = int(os.environ.get(DATABASE_URL, 'sqlite:///' + os.path.join(basedir, 'app.db')))
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
