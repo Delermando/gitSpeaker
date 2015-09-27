c491ae158ffac8208c7821b36037d3f945fc8970
Delermando <delsantos@hotmail.com.br>
2015-09-27 19:57:38 -0300

    teste

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_GOLD_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
+SQLALCHEMY_DATABASE_URI = "postgres://goppvwhsahqvlr:EJ7wGIxill234JW1z9PLsiPhFY@ec2-54-163-228-109.compute-1.amazonaws.com:5432/d5c7mhsq8h6jpl"
+#SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_GOLD_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
