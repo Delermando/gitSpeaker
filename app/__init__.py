65452318633baf6b4f9968c840a49b34268b3cd2
Delermando <delsantos@hotmail.com.br>
2015-09-27 13:33:36 -0300

    register SqlAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from core import *
 from github import Github
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
 Tools = Tools()
 Git = Git(Github)
 Content = Content()
 Section = Section()
 
-from app import views
+from app import views, models
