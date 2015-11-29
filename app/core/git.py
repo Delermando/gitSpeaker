59ee1efa4896d5afa79e4c2f4a2bde1726904e64
Delermando <delsantos@hotmail.com.br>
2015-11-29 10:40:56 -0200

    modify function to return file list

--- app/core/git.py
+++ app/core/git.py
@@ -1,4 +1,3 @@
-
 class Git(object):
 
     def __init__(self, Github):
@@ -7,49 +6,48 @@ class Git(object):
     def getGithubUser(self, userName, password):
         return self.Github(userName, password)
 
-    def sanitizeGitUrl( self, url ):
+    def sanitizeGitUrl(self, url):
         gitPath = url.split('/')
 
         if gitPath[-1] == '':
-           del gitPath[-1] 
+            del gitPath[-1]
 
-        if url.count('blob') > 0 or url.count('tree') > 0:    
+        if url.count('blob') > 0 or url.count('tree') > 0:
             del gitPath[2:4]
         return gitPath
 
-    def extractUserRepoInfo(self, path ):
+    def extractUserRepoInfo(self, path):
         result = {}
-        gitPath = self.sanitizeGitUrl( path )
+        gitPath = self.sanitizeGitUrl(path)
         result['user'] = gitPath[0]
         result['repository'] = gitPath[1]
         result['path'] = '/'.join(gitPath[2:])
         return result
 
-    def getSearcUser(self,git, username ):
+    def getSearcUser(self, git, username):
         try:
-            searchUser = git.legacy_search_users( username )
+            searchUser = git.legacy_search_users(username)
             for user in searchUser:
                 if user.login == username:
                     return user
         except Exception:
             return False
 
-
     def getRepository(self, user, repositoryName):
         try:
-            return user.get_repo( repositoryName )
+            return user.get_repo(repositoryName)
         except Exception:
             return False
 
     def getDirContentFromRepository(self, repository, dirName):
-        return repository.get_dir_contents( dirName )
+        return repository.get_dir_contents(dirName)
 
-    def getBranch(self, repository, branchName ):
-        return repository.get_branch( branchName )
+    def getBranch(self, repository, branchName):
+        return repository.get_branch(branchName)
 
     def getTree(self, repository, shaCommit, isRecursive):
         fileList = []
-        repoTree = repository.get_git_tree(shaCommit,isRecursive)
+        repoTree = repository.get_git_tree(shaCommit, isRecursive)
         for fl in repoTree.tree:
             fileList.append(fl)
         return fileList
@@ -65,31 +63,28 @@ class Git(object):
             return fileDict[filePath]
         else:
             return False
+
     def getFileContentsByList(self, repository, listfile):
         files = []
-        for key, value in listfile.iteritems(): 
+        for key, value in listfile.iteritems():
             files.append(self.getFileContent(repository, key))
         return files
 
-
     def getFileContent(self, repository, path):
-        return repository.get_file_contents( path )
+        return repository.get_file_contents(path)
 
-    def getDirContentFromRepository(self, repository, dirName):
-        return repository.get_dir_contents( dirName )
-
-    def getContents(self, username,password, searchUser, searchRepo, path, branchName, ignoreList):
+    def getFileList(self, username, password, searchUser, searchRepo, path, branchName, ignoreList):
         git = self.getGithubUser(username, password)
-        user = self.getSearcUser(git, searchUser )
-        repository = self.getRepository(user , searchRepo)
-        branch = self.getBranch( repository, branchName)
+        user = self.getSearcUser(git, searchUser)
+        repository = self.getRepository(user, searchRepo)
+        print(username, password)
+        branch = self.getBranch(repository, branchName)
         treeFiles = self.getTree(repository, branch.commit.sha, True)
         fileDict = self.extractFileListInfo(treeFiles)
         fileDictRemovedFolders = self.removeFolderFromList(fileDict)
         filesFromPath = self.getFilesFromPath(fileDictRemovedFolders, path)
-        print(filesFromPath)
-        clearDict = self.removeFilesInIgnoreList(filesFromPath, ignoreList)
-        return self.getFileContentsByList(repository, clearDict)
+        return self.removeFilesInIgnoreList(filesFromPath, ignoreList)
+        # return self.getFileContentsByList(repository, clearDict)
 
     def removeFolderFromList(self, filelist):
         result = {}
@@ -120,26 +115,25 @@ class Git(object):
         removelist = []
         for key, value in filelist.iteritems():
             if value == 'blob':
-                remove = self.getListFilesToRemove(key, ignorelist['files'] + ignorelist['extensions']+ignorelist['folders'])
+                remove = self.getListFilesToRemove(key, ignorelist['files'] + ignorelist['extensions'] + ignorelist['folders'])
 
             if remove:
                 removelist.append(key)
-        
         return self.removeFilesFromList(filelist, removelist)
 
-    def getListFilesToRemove( self, filename, fileRemove):
+    def getListFilesToRemove(self, filename, fileRemove):
         for fr in fileRemove:
             if filename.count(fr) >= 1:
                 return True
         return False
 
-    def getListFilesToRemoveByFolderName( self, filename, folderRemove):
+    def getListFilesToRemoveByFolderName(self, filename, folderRemove):
         for fr in folderRemove:
             if filename.count(fr) >= 1:
                 return True
         return False
 
-    def removeFilesFromList( self, filelist, listFilesToRemove):
+    def removeFilesFromList(self, filelist, listFilesToRemove):
         for fl in listFilesToRemove:
             del filelist[fl]
         return filelist
