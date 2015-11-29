255b29eaf0f7f06e8e19b8d2e22ed99a57e12deb
Delermando <delsantos@hotmail.com.br>
2015-11-29 10:41:28 -0200

    create route to response file list in json

--- app/views.py
+++ app/views.py
@@ -1,14 +1,13 @@
-from flask import render_template, jsonify
 from gitSpeaker import GitSpeaker
 from app import *
-import time
 
 # from flask import Markup
 # import base64
-# from app import models
 
 
 GitSpeaker = GitSpeaker()
+var = Tools.getEnviromentVar()
+ignorelist = Tools.getIgnoreList('ignorelist.json')
 
 
 @app.route("/")
@@ -16,21 +15,23 @@ def apresentation():
     return render_template('apresentation.html')
 
 
-response = {
-    "status_code": 200,
-    "message": "",
-    "data": {
-        "page": 'teste'
-    }
-}
-
-
 @app.route('/', defaults={'path': ''},  methods=['GET'])
 @app.route('/<path:path>',  methods=['GET'])
 def slide(path):
-    time.sleep(5)
-    return jsonify({"response": response})
+    urlParams = Git.extractUserRepoInfo(path)
+    GitSpeaker.saveUrlRepo(path)
+    gitFiles = Git.getFileList(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'], urlParams['user'], urlParams['repository'], urlParams['path'], 'master', ignorelist)
+    return GitSpeaker.returnFileList(200, "", gitFiles)
 
+
+
+@app.route('/v1/list/', defaults={'path': ''},  methods=['GET'])
+@app.route('/v1/list/<path:path>',  methods=['GET'])
+def fileList(path):
+    urlParams = Git.extractUserRepoInfo(path)
+    GitSpeaker.saveUrlRepo(path)
+    gitFiles = Git.getFileList(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'], urlParams['user'], urlParams['repository'], urlParams['path'], 'master', ignorelist)
+    return GitSpeaker.returnFileList(200, "", gitFiles)
 
 # @app.route('/wrapper/', defaults={'path': ''})
 # @app.route('/wrapper/<path:path>')
