a029261f8240968ef7cb9f1948960abd60a95b13
Delermando <delsantos@hotmail.com.br>
2015-11-29 03:39:03 -0200

    modify logic to a ajax

---
+++ app/views.bk.py
@@ -0,0 +1,55 @@
+from flask import render_template
+from gitSpeaker import GitSpeaker
+from app import *
+# from flask import Markup
+# import base64
+
+# from app import models
+
+
+GitSpeaker = GitSpeaker()
+
+@app.route("/")
+def apresentation():
+    return render_template('apresentation.html')
+
+@app.route('/wrapper/', defaults={'path': ''})
+@app.route('/wrapper/<path:path>')
+def wrapper(path):
+    var = Tools.getEnviromentVar()
+    urlParams = Git.extractUserRepoInfo(path)
+    ignorelist = Tools.getIgnoreList('ignorelist.json')
+    GitSpeaker.saveUrlRepo(path)
+
+    if var['GITSPEAKER_GH_ENVIRONMENT'] == 'prod':
+        gitFiles = Git.getContents(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'], urlParams['user'], urlParams['repository'], urlParams['path'], 'master', ignorelist)
+        response = GitSpeaker.getWrapperFileContents(gitFiles)
+    else:
+        fileList = Tools.getLocalFileList(path)
+        filesContents = []
+        for flPath in fileList:
+            filesContents.append({'name':flPath, 'contents': Tools.getLocalContent(flPath)})
+        response = GitSpeaker.getWrapperLocalFileContents(filesContents,int(var['GITSPEAKER_GH_MARKDOWNLINESNUMBER']), int(var['GITSPEAKER_GH_CODELINESNUMBER']))
+    return render_template('slide.html', content = response)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def slide(path):
+    var = Tools.getEnviromentVar()
+    ignorelist = Tools.getIgnoreList('ignorelist.json')
+    urlParams = Git.extractUserRepoInfo(path)
+    GitSpeaker.saveUrlRepo(path)
+
+    if var['GITSPEAKER_GH_ENVIRONMENT'] == 'prod':
+        gitFiles = Git.getContents(var['GITSPEAKER_GH_USERNAME'], var['GITSPEAKER_GH_PASSWORD'], urlParams['user'], urlParams['repository'], urlParams['path'], 'master', ignorelist)
+        print(gitFiles)
+        response = GitSpeaker.getFileContents(gitFiles)     
+    else:
+        fileList = Tools.getLocalFileList(path)
+        filesContents = []
+        for flPath in fileList:
+            filesContents.append({'name':flPath, 'contents': Tools.getLocalContent(flPath)})
+        response = GitSpeaker.getLocalFileContents(filesContents)
+    return render_template('slide.html', content = response)
+
