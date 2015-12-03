c3b13a9beeb80e37b86062cce52204d595a1d7b9
Delermando <delsantos@hotmail.com.br>
2015-12-03 21:57:38 -0200

    adicionando suporte ao memcache

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import os
 from app import app
+from werkzeug.contrib.cache import MemcachedCache
+
+
 port = int(os.environ.get("PORT", 4000))
 app.run(debug = True, host='0.0.0.0', port=port)
+cache = MemcachedCache(['0.0.0.0:11211'])
