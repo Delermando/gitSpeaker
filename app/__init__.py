dc10cdb41ac3f120aea6550d9e25c389a2c17543
Delermando <delsantos@hotmail.com.br>
2015-12-04 01:42:40 -0200

    teste logs

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from core import *
 from github import Github
 from flask.ext.sqlalchemy import SQLAlchemy
+import sys
+import logging
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 db = SQLAlchemy(app)
 
 Tools = Tools()
